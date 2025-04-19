d3e080687fd0fcf8e6a9ad6add23ea95b3b25522
Codemcm <codemcm@hotmail.com>
2025-04-18 19:40:36 -0600

    datacontext added

---
+++ DataQuiz/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataQuiz.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly string _connectionString;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, string connectionString) :
+            base(options)
+        {
+            this._connectionString = connectionString;
+        }
+    }
+}
