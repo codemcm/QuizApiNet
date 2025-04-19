9d9c5c33dab17acc5f05002332e4f7b0cde43f7c
Codemcm <codemcm@hotmail.com>
2025-04-18 19:30:27 -0600

    inicia proyecto

---
+++ Dto/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto
+{
+    public  class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public String NombreCategoria { get; set; }
+        public bool Active { get; set; }    
+    }
+}
