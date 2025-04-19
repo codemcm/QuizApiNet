631210ed81e6f9047695f565b8e3542e3fb5a78d
Codemcm <codemcm@hotmail.com>
2025-04-18 20:01:29 -0600

    agregar entity para categoria

---
+++ DataQuiz/CategoriaEntity.cs
@@ -0,0 +1,34 @@
+﻿using DataQuiz.Context;
+using Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataQuiz
+{
+    public class CategoriaEntity
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriaEntity(string connectionString)
+        {
+            _context = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>(), connectionString);
+        }
+        public async Task<List<Categoria>> GetAllCategoriasAsync()
+        {
+            return await _context.Categorias.ToListAsync();
+        }
+
+        public async Task<Categoria> AddNew(Categoria categoria)
+        {
+            var categoriaNew = _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
+            return categoriaNew.Entity;
+        }
+
+        public async Task<Categoria> Update(Categoria categoria)
+        {
+            var categoriaUpdate = _context.Categorias.Update(categoria);
+            await _context.SaveChangesAsync();
+            return categoriaUpdate.Entity;
+        }
+    }
+}
